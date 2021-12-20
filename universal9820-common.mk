44f84de07d2fbf6d0eee553cab607aa7fe8557a6
Lunarixus <Nathan@lunarixus.dev>
2021-12-20 16:51:28 +0000

    universal9820-common: Build RIL packages
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- universal9820-common.mk
+++ universal9820-common.mk
@@ -182,6 +182,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    libxml2
+
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
