42ffb27a6a84fd8786081f36e28556734e678985
Lunarixus <Nathan@lunarixus.dev>
2021-12-20 16:40:25 +0000

    universal9820-common: Build Bluetooth packages
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- universal9820-common.mk
+++ universal9820-common.mk
@@ -40,6 +40,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    audio.a2dp.default
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
