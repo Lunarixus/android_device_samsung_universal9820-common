012ebe14745308f425c0c115e21dca4afdba5461
Lunarixus <Nathan@lunarixus.dev>
2021-12-20 16:27:39 +0000

    universal9820-common: Build configstore packages
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- universal9820-common.mk
+++ universal9820-common.mk
@@ -52,6 +52,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4 \
     Snap
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-impl \
+    android.hardware.configstore@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
